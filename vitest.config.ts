8d0fe6f69fb0c3c02d8c3163c259180c26cb502d
Jhonny Gil <gilsjhonny@Jhonnys-MacBook-Pro.local>
2024-08-23 23:08:48 +0200

    Add coverage tool and update configurations
    Added coverage tool, updated .gitignore to exclude the coverage folder, excluded config files and types from coverage, and updated prettierrc.

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,8 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     setupFiles: './src/tests/setup.ts',
+    coverage: {
+      exclude: ['**/types.ts', '**/*.d.ts', '**/setup.ts', './vitest.config.ts', './eslint.config.js'],
+    },
   },
 });
