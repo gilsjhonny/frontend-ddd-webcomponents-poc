9fc3258c03d7c234bb1a1ed4f41055cf947364e7
Jhonny Gil <gilsjhonny@Jhonnys-MacBook-Pro.local>
2024-08-25 12:07:30 +0200

    Update shadow dom utils

--- src/tests/shadow-dom-utils.ts
+++ src/tests/shadow-dom-utils.ts
@@ -1,5 +1,13 @@
-import { getByText } from '@testing-library/dom';
+import { getByRole, getByText, getByTestId } from '@testing-library/dom';
 
 export function getByTextFromShadowRoot(shadowRoot: ShadowRoot, text: string, exact: boolean = false) {
   return getByText(shadowRoot as unknown as HTMLElement, text, { exact });
+}
+
+export function getByRoleFromShadowRoot(shadowRoot: ShadowRoot, role: string) {
+  return getByRole(shadowRoot as unknown as HTMLElement, role);
+}
+
+export function getByTestIdFromShadowRoot(shadowRoot: ShadowRoot, testId: string) {
+  return getByTestId(shadowRoot as unknown as HTMLElement, testId);
 }
